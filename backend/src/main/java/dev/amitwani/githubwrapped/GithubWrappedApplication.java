36e920599011107fb3d5aabb9f4682c0707338b5
Amit Wani <mtwn105@gmail.com>
2024-12-14 16:54:22 +0530

    Initial commit

---
+++ backend/src/main/java/dev/amitwani/githubwrapped/GithubWrappedApplication.java
@@ -0,0 +1,13 @@
+package dev.amitwani.githubwrapped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWrappedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubWrappedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/amitwani/githubwrapped/GithubWrappedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amitwani.githubwrapped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWrappedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
