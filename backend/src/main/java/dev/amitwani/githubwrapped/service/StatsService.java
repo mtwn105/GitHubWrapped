d792589dbfcec3d3f422ff4fc4d90633b0f7f151
Amit Wani <mtwn105@gmail.com>
2024-12-22 13:00:49 +0530

    refactoring

--- backend/src/main/java/dev/amitwani/githubwrapped/service/StatsService.java
+++ backend/src/main/java/dev/amitwani/githubwrapped/service/StatsService.java
@@ -1,38 +1,36 @@
 package dev.amitwani.githubwrapped.service;
 
-import dev.amitwani.githubwrapped.dto.AllUserDTO;
-import dev.amitwani.githubwrapped.dto.ResponseDTO;
-import dev.amitwani.githubwrapped.dto.StatsDTO;
-import dev.amitwani.githubwrapped.dto.TopUserDTO;
-import dev.amitwani.githubwrapped.dto.graphql.GitHubContributionStats;
-import dev.amitwani.githubwrapped.dto.graphql.GitHubPinnedItems;
-import dev.amitwani.githubwrapped.dto.graphql.GitHubRepositoryStats;
-import dev.amitwani.githubwrapped.model.GitHubStats;
-import dev.amitwani.githubwrapped.model.GitHubUser;
-import dev.amitwani.githubwrapped.repository.GitHubStatsRepository;
-import dev.amitwani.githubwrapped.repository.GitHubUserRepository;
+import dev.amitwani.githubwrapped.dto.*;
+import dev.amitwani.githubwrapped.dto.graphql.*;
+import dev.amitwani.githubwrapped.model.*;
+import dev.amitwani.githubwrapped.repository.*;
 import org.kohsuke.github.GHUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class StatsService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StatsService.class);
 
-    @Autowired
-    private GitHubService gitHubService;
+    private final GitHubService gitHubService;
+    private final GitHubUserRepository gitHubUserRepository;
+    private final GitHubStatsRepository gitHubStatsRepository;
 
     @Autowired
-    private GitHubUserRepository gitHubUserRepository;
-
-    @Autowired
-    private GitHubStatsRepository gitHubStatsRepository;
+    public StatsService(GitHubService gitHubService, GitHubUserRepository gitHubUserRepository, GitHubStatsRepository gitHubStatsRepository) {
+        this.gitHubService = gitHubService;
+        this.gitHubUserRepository = gitHubUserRepository;
+        this.gitHubStatsRepository = gitHubStatsRepository;
+    }
 
     public ResponseEntity<ResponseDTO> generateGitHubStats(String username) {
         try {
@@ -41,131 +39,14 @@ public class StatsService {
                 LOGGER.info("User {} stats already exists", username);
                 return ResponseEntity.ok(new ResponseDTO("User stats already exists", statsDTO));
             }
-            GHUser user;
-            try {
-                // Fetch User Data from GitHub
-                user = gitHubService.getGitHubUser(username.toLowerCase());
-                LOGGER.info("Fetched user data for user: {}", user);
 
-                if (user == null) {
-                    LOGGER.error("No user data found for user: {}", username);
-                    return ResponseEntity.status(404).body(new ResponseDTO("No user data found", null));
-                }
-
-            } catch (Exception e) {
-                LOGGER.error("Failed to fetch user data for user: {}", username, e);
+            GHUser user = fetchGitHubUser(username);
+            if (user == null) {
                 return ResponseEntity.status(404).body(new ResponseDTO("No user data found", null));
             }
 
-            // Process User Data
-            GitHubUser gitHubUser = new GitHubUser();
-            gitHubUser.setBio(user.getBio());
-            gitHubUser.setCompany(user.getCompany());
-            gitHubUser.setEmail(user.getEmail());
-            gitHubUser.setAvatarUrl(user.getAvatarUrl());
-            gitHubUser.setBlogUrl(user.getBlog());
-            gitHubUser.setFollowers(user.getFollowersCount());
-            gitHubUser.setFollowing(user.getFollowingCount());
-            gitHubUser.setPublicRepos(user.getPublicRepoCount());
-            gitHubUser.setName(user.getName());
-            gitHubUser.setTwitterUsername(user.getTwitterUsername());
-            gitHubUser.setUsername(username.toLowerCase());
-
-            // Get User Pinned Repos
-            GitHubPinnedItems pinnedRepos = gitHubService.getPinnedRepos(username.toLowerCase());
-            LOGGER.info("Fetched pinned repos for user: {}", pinnedRepos);
-
-            // Save Pinned Repos
-            List<GitHubPinnedItems.Node> pinnedRepoNodes = pinnedRepos.getData().getUser().getPinnedItems().getEdges().stream().map(edge -> edge.getNode()).toList();
-
-            for (GitHubPinnedItems.Node node : pinnedRepoNodes) {
-                gitHubUser.getPinnedRepositories().add(new GitHubUser.PinnedRepositories(
-                        node.getName(),
-                        node.getDescription(),
-                        node.getUrl(),
-                        node.getStars(),
-                        node.getForkCount(),
-                        node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getName() : null,
-                        node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getColor() : null
-                ));
-            }
-
-            // Generate Stats
-            GitHubStats gitHubStats = new GitHubStats();
-            gitHubStats.setUsername(username.toLowerCase());
-
-            List<GitHubRepositoryStats.RepositoryNode> repositoryNodes = gitHubService.getRepositoryStats(username.toLowerCase());
-
-            // Languages
-            List<GitHubRepositoryStats.LanguageEdge> languageEdges = repositoryNodes
-                    .stream()
-                    .flatMap(node -> node.getLanguages().getEdges()
-                            .stream()
-                            .toList()
-                            .stream())
-                    .toList();
-
-
-            Map<String, GitHubStats.LanguageStats> languageStatsMap = new HashMap<>();
-
-            languageEdges.forEach(edge -> {
-                if (languageStatsMap.containsKey(edge.getNode().getName())) {
-                    GitHubStats.LanguageStats languageStats = languageStatsMap.get(edge.getNode().getName());
-                    languageStats.setLinesCount(languageStats.getLinesCount() + edge.getSize());
-                    return;
-                }
-                GitHubStats.LanguageStats languageStats = new GitHubStats.LanguageStats();
-                languageStats.setLanguage(edge.getNode().getName());
-                languageStats.setColor(edge.getNode().getColor());
-                languageStats.setLinesCount(edge.getSize());
-                languageStatsMap.put(edge.getNode().getName(), languageStats);
-            });
-
-            List<GitHubStats.LanguageStats> languageStats = languageStatsMap.values().stream()
-                    .sorted(Comparator.comparing(GitHubStats.LanguageStats::getLinesCount).reversed())
-                    .toList();
-
-            gitHubStats.setLanguagesStats(languageStats);
-
-            // Get Contribution Stats
-            GitHubContributionStats contributionStats = gitHubService.getContributionStats(username.toLowerCase());
-
-            gitHubStats.setTotalCommits(contributionStats.getData().getUser().getContributionsCollection().getCommits());
-            gitHubStats.setTotalIssuesClosed(contributionStats.getData().getUser().getContributionsCollection().getIssuesClosed());
-            gitHubStats.setTotalPullRequestsClosed(contributionStats.getData().getUser().getContributionsCollection().getPullRequestsClosed());
-            gitHubStats.setContributionCalendar(contributionStats.getData().getUser().getContributionsCollection().getContributionCalendar());
-
-            // Top Repository
-            GitHubStats.Repository topRepository = new GitHubStats.Repository();
-
-            GitHubRepositoryStats.RepositoryNode topRepositoryNode = repositoryNodes.stream()
-                    .max(Comparator.comparing(GitHubRepositoryStats.RepositoryNode::getStars))
-                    .orElse(null);
-
-            if (topRepositoryNode != null) {
-                topRepository.setName(topRepositoryNode.getName());
-                topRepository.setTopLanguage(topRepositoryNode.getPrimaryLanguage() != null ? topRepositoryNode.getPrimaryLanguage().getName() : null);
-                topRepository.setTopLanguageColor(topRepositoryNode.getPrimaryLanguage() != null ? topRepositoryNode.getPrimaryLanguage().getColor() : null);
-                topRepository.setStars(topRepositoryNode.getStars());
-                topRepository.setForks(topRepositoryNode.getForkCount());
-                gitHubStats.setTopRepository(topRepository);
-            }
-
-            // Total Stars
-            gitHubStats.setTotalStars(repositoryNodes.stream().mapToInt(GitHubRepositoryStats.RepositoryNode::getStars).sum());
-
-            // Total Forks
-            gitHubStats.setTotalForks(repositoryNodes.stream().mapToInt(GitHubRepositoryStats.RepositoryNode::getForkCount).sum());
-
-            // Save User Data
-            gitHubUser.setCreatedDate(new Date());
-            gitHubUser = gitHubUserRepository.save(gitHubUser);
-            LOGGER.info("Saved user data for user: {}", gitHubUser);
-
-            // Save Stats
-            gitHubStats.setUserId(gitHubUser.getId());
-            gitHubStats = gitHubStatsRepository.save(gitHubStats);
-            LOGGER.info("Generated stats for user: {}", gitHubStats);
+            GitHubUser gitHubUser = processGitHubUser(user, username);
+            GitHubStats gitHubStats = generateStats(username, gitHubUser);
 
             statsDTO = new StatsDTO();
             statsDTO.setStats(gitHubStats);
@@ -175,12 +56,108 @@ public class StatsService {
             return ResponseEntity.status(201).body(new ResponseDTO("Stats generated successfully", statsDTO));
         } catch (Exception e) {
             LOGGER.error("Error generating stats for user: {}", username, e);
+            return ResponseEntity.internalServerError().body(new ResponseDTO("Error generating stats", null));
         }
-        return ResponseEntity.internalServerError().body(new ResponseDTO("Error generating stats", null));
+    }
+
+    private GHUser fetchGitHubUser(String username) {
+        try {
+            GHUser user = gitHubService.getGitHubUser(username.toLowerCase());
+            LOGGER.info("Fetched user data for user: {}", user);
+            return user;
+        } catch (Exception e) {
+            LOGGER.error("Failed to fetch user data for user: {}", username, e);
+            return null;
+        }
+    }
+
+    private GitHubUser processGitHubUser(GHUser user, String username) throws IOException {
+        GitHubUser gitHubUser = new GitHubUser();
+        gitHubUser.setBio(user.getBio());
+        gitHubUser.setCompany(user.getCompany());
+        gitHubUser.setEmail(user.getEmail());
+        gitHubUser.setAvatarUrl(user.getAvatarUrl());
+        gitHubUser.setBlogUrl(user.getBlog());
+        gitHubUser.setFollowers(user.getFollowersCount());
+        gitHubUser.setFollowing(user.getFollowingCount());
+        gitHubUser.setPublicRepos(user.getPublicRepoCount());
+        gitHubUser.setName(user.getName());
+        gitHubUser.setTwitterUsername(user.getTwitterUsername());
+        gitHubUser.setUsername(username.toLowerCase());
+
+        GitHubPinnedItems pinnedRepos = gitHubService.getPinnedRepos(username.toLowerCase());
+        LOGGER.info("Fetched pinned repos for user: {}", pinnedRepos);
+
+        List<GitHubPinnedItems.Node> pinnedRepoNodes = pinnedRepos.getData().getUser().getPinnedItems().getEdges().stream()
+                .map(GitHubPinnedItems.Edge::getNode)
+                .toList();
+
+        for (GitHubPinnedItems.Node node : pinnedRepoNodes) {
+            gitHubUser.getPinnedRepositories().add(new GitHubUser.PinnedRepositories(
+                    node.getName(),
+                    node.getDescription(),
+                    node.getUrl(),
+                    node.getStars(),
+                    node.getForkCount(),
+                    node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getName() : null,
+                    node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getColor() : null
+            ));
+        }
+
+        gitHubUser.setCreatedDate(new Date());
+        return gitHubUserRepository.save(gitHubUser);
+    }
+
+    private GitHubStats generateStats(String username, GitHubUser gitHubUser) {
+        GitHubStats gitHubStats = new GitHubStats();
+        gitHubStats.setUsername(username.toLowerCase());
+
+        List<GitHubRepositoryStats.RepositoryNode> repositoryNodes = gitHubService.getRepositoryStats(username.toLowerCase());
+
+        Map<String, GitHubStats.LanguageStats> languageStatsMap = repositoryNodes.stream()
+                .flatMap(node -> node.getLanguages().getEdges().stream())
+                .collect(Collectors.toMap(
+                        edge -> edge.getNode().getName(),
+                        edge -> new GitHubStats.LanguageStats(edge.getNode().getName(), edge.getNode().getColor(), edge.getSize()),
+                        (existing, replacement) -> {
+                            existing.setLinesCount(existing.getLinesCount() + replacement.getLinesCount());
+                            return existing;
+                        }
+                ));
+
+        List<GitHubStats.LanguageStats> languageStats = languageStatsMap.values().stream()
+                .sorted(Comparator.comparing(GitHubStats.LanguageStats::getLinesCount).reversed())
+                .collect(Collectors.toList());
+
+        gitHubStats.setLanguagesStats(languageStats);
+
+        GitHubContributionStats contributionStats = gitHubService.getContributionStats(username.toLowerCase());
+
+        gitHubStats.setTotalCommits(contributionStats.getData().getUser().getContributionsCollection().getCommits());
+        gitHubStats.setTotalIssuesClosed(contributionStats.getData().getUser().getContributionsCollection().getIssuesClosed());
+        gitHubStats.setTotalPullRequestsClosed(contributionStats.getData().getUser().getContributionsCollection().getPullRequestsClosed());
+        gitHubStats.setContributionCalendar(contributionStats.getData().getUser().getContributionsCollection().getContributionCalendar());
+
+        GitHubStats.Repository topRepository = repositoryNodes.stream()
+                .max(Comparator.comparing(GitHubRepositoryStats.RepositoryNode::getStars))
+                .map(node -> new GitHubStats.Repository(
+                        node.getName(),
+                        node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getName() : null,
+                        node.getPrimaryLanguage() != null ? node.getPrimaryLanguage().getColor() : null,
+                        node.getStars(),
+                        node.getForkCount()
+                ))
+                .orElse(null);
+
+        gitHubStats.setTopRepository(topRepository);
+        gitHubStats.setTotalStars(repositoryNodes.stream().mapToInt(GitHubRepositoryStats.RepositoryNode::getStars).sum());
+        gitHubStats.setTotalForks(repositoryNodes.stream().mapToInt(GitHubRepositoryStats.RepositoryNode::getForkCount).sum());
+
+        gitHubStats.setUserId(gitHubUser.getId());
+        return gitHubStatsRepository.save(gitHubStats);
     }
 
     public StatsDTO getStats(String username) {
-
         List<GitHubUser> gitHubUserList = gitHubUserRepository.findByUsername(username.toLowerCase());
         if (gitHubUserList == null || gitHubUserList.isEmpty()) {
             LOGGER.info("User not found for username: {}", username);
@@ -202,17 +179,15 @@ public class StatsService {
     }
 
     public List<TopUserDTO> getTopUsers() {
-
-
         List<TopUserDTO> topUserList = new ArrayList<>();
-
         List<GitHubStats> gitHubStatsList = gitHubStatsRepository.findTop6ByOrderByTotalCommitsDesc();
 
         LOGGER.info("Fetched top users: {}", gitHubStatsList.size());
 
-        gitHubStatsList.forEach(gitHubStats -> {
+        for (GitHubStats gitHubStats : gitHubStatsList) {
             TopUserDTO topUser = new TopUserDTO();
             topUser.setUsername(gitHubStats.getUsername());
+
             List<GitHubUser> gitHubUserList = gitHubUserRepository.findByUsername(gitHubStats.getUsername().toLowerCase());
             if (gitHubUserList != null && !gitHubUserList.isEmpty()) {
                 topUser.setName(gitHubUserList.getFirst().getName());
@@ -227,7 +202,7 @@ public class StatsService {
             topUser.setTotalStars(gitHubStats.getTotalStars());
             topUser.setTotalForks(gitHubStats.getTotalForks());
             topUserList.add(topUser);
-        });
+        }
 
         topUserList.sort(Comparator.comparing(TopUserDTO::getTotalContributions).reversed());
 
